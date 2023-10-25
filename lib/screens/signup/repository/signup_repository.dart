a6c90f8f9a6291136f35da009482517e3346d504
CDevFlttr <marraffacinzia5@gmail.com>
2023-10-25 11:44:47 +0200

    modific

--- lib/screens/signup/repository/signup_repository.dart
+++ lib/screens/signup/repository/signup_repository.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 import 'package:anf_app/api_response/model/api_response_model.dart';
 import 'package:anf_app/secure_storage/secure_storage.dart';
@@ -9,35 +8,35 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../../signin/page/signin_page.dart';
 
 class SignupRepository {
-
-  Future registerUser(BuildContext context, String nome, String email, String password, String confirmPassword, String richiesta) async{
+  Future registerUser(BuildContext context, String nome, String email,
+      String password, String confirmPassword, String richiesta) async {
     try {
-             
+      var url = '${dotenv.env['NEXT_PUBLIC_BACKEND_URL']!}/api/register';
+      // Await the http get response, then decode the json-formatted response.
+      var response = await http.post(Uri.parse(url),
+          headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+          },
+          body: jsonEncode({
+            'nome': nome,
+            'email': email,
+            'password': password,
+            'password_confirmation': confirmPassword,
+            'richiesta': richiesta
+          }));
+      print('signup ${response.statusCode}');
 
-var url = '${dotenv.env['NEXT_PUBLIC_BACKEND_URL']!}/api/register';
-  // Await the http get response, then decode the json-formatted response.
-  var response = await http.post(Uri.parse(url),
-  headers: {
-    'Accept': 'application/json',
-    'Content-type': 'application/json',
-  },
-  body: jsonEncode({
-    'nome': nome,
-    'email': email,
-    'password': password,
-    'password_confirmation': confirmPassword,
-    'richiesta': richiesta
-  }));
- if (response.statusCode == 200) {
-               print('signup ${jsonDecode(response.body)}');
+      if (response.statusCode == 200) {
+        print('signup ${jsonDecode(response.body)}');
 
-Navigator.pushReplacement(            context, MaterialPageRoute(builder: (_) =>  SignInPage()));
-
+        // ignore: use_build_context_synchronously
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (_) => SignInPage()));
       } else {
         print('response ${response.statusCode}');
       }
- return response;
-   
+      return response;
     } catch (e) {
       print('sendimage error $e');
     }
