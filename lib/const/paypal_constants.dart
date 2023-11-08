598f818c86214d1c4fe6df51bc85310db4de554d
CDevFlttr <marraffacinzia5@gmail.com>
2023-11-08 11:43:07 +0100

    modific

--- lib/const/paypal_constants.dart
+++ lib/const/paypal_constants.dart
@@ -1,7 +1,7 @@
 
 
 class PaypalConstants {
-  static const String clientId = 'Ad1ip36GVpfTTKv72rdlRv0bTNwaPFCfrAYkkNU8eeU76dN0PgZDkAzvMNA05yGuWKVEg3ltWCrDz_Rh';
-    static const String secretKey = 'EBiGR8BYHvzcHNGCPmJMbaRXOe_0cdD--9kYw6E7ukThmZBH_feqne0hfWQZ4TFCbOUynuso7IzPCvrb';
+  static const String clientId = 'AdB5F3PI8pHJIreNTOKMABXrS3HNXxWClzOnVMG-YCEDVDiP_19bo2c5-j6xBvlIKNf4qk_B2rlGMFwE';
+    static const String secretKey = 'EJ6hAEaOf9SUDgYXKQd-lvoYxXgMpHSIxPsLBsbnh05e64Wd0rX3jvLnOHwe7qbKAJBUanWCTS6IankZ';
 
 }
--- lib/screens/presentation/widgets/custom_cards.dart
+++ lib/screens/presentation/widgets/custom_cards.dart
@@ -3,7 +3,7 @@ import 'package:app_solidale/const/path_constants.dart';
 import 'package:app_solidale/screens/common_widgets/custom_cards_common.dart';
 
 import 'package:app_solidale/screens/presentation/widgets/custom_container_service.dart';
-import 'package:flutter_paypal/flutter_paypal.dart';
+import 'package:flutter_paypal_checkout/flutter_paypal_checkout.dart';
 import 'package:flutter/material.dart';
 
 import '../../../const/paypal_constants.dart';
@@ -13,10 +13,9 @@ import 'already_account.dart';
 // ignore: must_be_immutable
 class CustomCard extends StatelessWidget {
   String titleOffroAiuto = "Offro Aiuto";
-    String titleChiedoAiuto = "Chiedo Aiuto";
+  String titleChiedoAiuto = "Chiedo Aiuto";
 
   CustomCard({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -30,18 +29,19 @@ class CustomCard extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Flexible(
-            
             child: Column(
               children: [
                 GestureDetector(
                   onTap: () {
                     Navigator.push(
-                        context, MaterialPageRoute(builder: (_) => SignUpPage(
-                         service: titleOffroAiuto,
-                        )));
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => SignUpPage(
+                                  service: titleOffroAiuto,
+                                )));
                   },
                   child: CustomCardsCommon(
-                    child:  CustomContainerService(
+                    child: CustomContainerService(
                       title: titleOffroAiuto,
                       subtitle:
                           'Crea il tuo Account per donare il tuo tempo ad ANF',
@@ -55,12 +55,14 @@ class CustomCard extends StatelessWidget {
                 GestureDetector(
                   onTap: () {
                     Navigator.push(
-                        context, MaterialPageRoute(builder: (_) => SignUpPage(
-                          service: titleChiedoAiuto,
-                        )));
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => SignUpPage(
+                                  service: titleChiedoAiuto,
+                                )));
                   },
                   child: CustomCardsCommon(
-                    child:  CustomContainerService(
+                    child: CustomContainerService(
                       title: titleChiedoAiuto,
                       subtitle:
                           'Crea il tuo Account per usufruire dei servizi ANF',
@@ -71,9 +73,7 @@ class CustomCard extends StatelessWidget {
                 const SizedBox(
                   height: 30,
                 ),
-                CustomCardsCommon(
-                  child: alreadyAccount(context)
-                ),
+                CustomCardsCommon(child: alreadyAccount(context)),
               ],
             ),
           ),
@@ -85,71 +85,69 @@ class CustomCard extends StatelessWidget {
                 style: TextStyle(color: Colors.black, fontSize: 16),
               ),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => UsePaypal(
-                            sandboxMode: true,
-                            clientId:
-                                PaypalConstants.clientId,
-                            secretKey:
-                                 PaypalConstants.secretKey,
-                            returnURL: "https://samplesite.com/return",
-                            cancelURL: "https://samplesite.com/cancel",
-                            transactions: const [
-                              {
-                                "amount": {
-                                  "total": '10.12',
-                                  "currency": "USD",
-                                  "details": {
-                                    "subtotal": '10.12',
-                                    "shipping": '0',
-                                    "shipping_discount": 0
-                                  }
-                                },
-                                "description":
-                                    "The payment transaction description.",
-                                // "payment_options": {
-                                //   "allowed_payment_method":
-                                //       "INSTANT_FUNDING_SOURCE"
-                                // },
-                                "item_list": {
-                                  "items": [
-                                    {
-                                      "name": "A demo product",
-                                      "quantity": 1,
-                                      "price": '10.12',
-                                      "currency": "USD"
-                                    }
-                                  ],
+                onPressed: () async {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => PaypalCheckout(
+                sandboxMode: true,
+                clientId: PaypalConstants.clientId,
+                secretKey: PaypalConstants.secretKey,
+                returnURL: "success.snippetcoder.com",
+                cancelURL: "cancel.snippetcoder.com",
+                transactions: const [
+                  {
+                    "amount": {
+                      "total": '10.00',
+                      "currency": "EUR",
+                      "details": {
+                        "subtotal": '10.00',
+                        "shipping": '0',
+                        "shipping_discount": 0
+                      }
+                    },
+                    "description": "The payment transaction description.",
+                    // "payment_options": {
+                    //   "allowed_payment_method":
+                    //       "INSTANT_FUNDING_SOURCE"
+                    // },
+                    "item_list": {
+                      "items": [
+                        {
+                          "name": "Apple",
+                          "quantity": 1,
+                          "price": '10.00',
+                          "currency": "EUR"
+                        },
+                        
+                      ],
 
-                                  // shipping address is not required though
-                                  "shipping_address": {
-                                    "recipient_name": "Jane Foster",
-                                    "line1": "Travis County",
-                                    "line2": "",
-                                    "city": "Austin",
-                                    "country_code": "US",
-                                    "postal_code": "73301",
-                                    "phone": "+00000000",
-                                    "state": "Texas"
-                                  },
-                                }
-                              }
-                            ],
-                            note: "Contact us for any questions on your order.",
-                            onSuccess: (Map params) async {
-                              print("onSuccess: $params");
-                            },
-                            onError: (error) {
-                              print("onError: $error");
-                            },
-                            onCancel: (params) {
-                              print('cancelled: $params');
-                            }),
-                      ),
-                    );
+                      // shipping address is not required though
+                         "shipping_address": {
+                           "recipient_name": "Raman Singh",
+                           "line1": "Delhi",
+                           "line2": "",
+                           "city": "Delhi",
+                           "country_code": "IN",
+                           "postal_code": "11001",
+                           "phone": "+00000000",
+                           "state": "Texas"
+                        },
+                    }
+                  }
+                ],
+                note: "Contact us for any questions on your order.",
+                onSuccess: (Map params) async {
+                  print("onSuccess: $params");
                 },
+                onError: (error) {
+                  print("onError: $error");
+                  Navigator.pop(context);
+                },
+                onCancel: () {
+                  print('cancelled:');
+                },
+              ),
+            ));
+          },
                 style: ElevatedButton.styleFrom(
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20.0)),
