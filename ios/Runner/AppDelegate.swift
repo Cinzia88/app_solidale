a306c3a703f2a92d5fbccfd1e0ffab7d6c2d2fd8
marraffacinzia5@gmail.com <marraffacinzia5@gmail.com>
2024-03-13 12:29:50 +0100

    modific

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,8 @@
 import UIKit
 import Flutter
 import flutter_local_notifications
-import Firebase
+import FirebaseCore
+import FirebaseMessaging
 
 
 @UIApplicationMain
@@ -10,19 +11,76 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-   
+    
+          
+      FirebaseApp.configure()
+      // Push Notifications
+          
+          if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+              options: authOptions,
+              completionHandler: { _, _ in }
+            )
+          } else {
+            let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+          }
+          
+          application.registerForRemoteNotifications()
+          
+          // Messaging Delegate
+          
+          Messaging.messaging().delegate = self
 
-     FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-        GeneratedPluginRegistrant.register(with: registry)
-    }
+      FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+         GeneratedPluginRegistrant.register(with: registry)
+          
+      }
 
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
     }
-     FirebaseApp.configure()
-      application.registerForRemoteNotifications()
+     
       
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+
+    super.application(application,didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+    }
+    
+    
 }
+
+
+
+
+// MARK: - UNUserNotificationCenterDelegate
+
+
+
+
+extension AppDelegate: MessagingDelegate {
+  
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+
+      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+      NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+      // TODO: If necessary send token to application server.
+      // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+    
+  }
